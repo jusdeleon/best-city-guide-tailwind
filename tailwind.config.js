4a579f4482d8b20ce6a398070e8a86efeed4340e
Justin De Leon <jusmdeleon@gmail.com>
2021-02-21 22:59:24 +0800

    Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Varela Round', 'sans-serif'],
+      serif: ['serif'],
+    },
+    extend: {
+      colors: {
+        teal: colors.teal
+      }
+    }
   },
   variants: {
     extend: {},
